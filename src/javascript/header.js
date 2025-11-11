a6cf67bf135358a955be188a74e85430fff2e7d1
sanchit raizada <sanchit_raizada@hotmail.com>
2025-11-11 18:51:48 +0530

    all the changes are made

--- src/javascript/header.js
+++ src/javascript/header.js
@@ -1,57 +1,3 @@
-// export function initHeader() {
-//     var toggleOpen = document.getElementById('toggleOpen');
-//     var toggleClose = document.getElementById('toggleClose');
-//     var collapseMenu = document.getElementById('collapseMenu');
-
-//     function handleClick() {
-//         if (collapseMenu.style.display === 'flex') {
-//             // collapseMenu.style.display = 'none';
-//             // toggleOpen.style.display="flex";
-//             toggleClose.style.display="none";
-
-//         } else {
-//             // collapseMenu.style.display = 'flex';
-//             // toggleOpen.style.display="none"
-//             toggleClose.style.display="flex"
-
-//         }
-//     }
-
-
-
-//     toggleOpen.addEventListener('click', handleClick);
-//     toggleClose.addEventListener('click', handleClick);
-
-//     // for mega menu
-//     const toggleMegaMenu = document.getElementById('toggle-mega-menu');
-//     const megaMenu = document.getElementById('mega-menu');
-
-//     // Pehle se mega menu ko hidden karo
-//     megaMenu.classList.add('hidden');
-
-//     toggleMegaMenu.addEventListener('click', (e) => {
-//         e.stopPropagation();
-//         megaMenu.classList.toggle('hidden');
-//         toggleMegaMenu.parentElement.classList.toggle('border-transparent');
-//         toggleMegaMenu.classList.toggle('text-slate-900');
-//         toggleMegaMenu.classList.toggle('text-slate-600');
-//     });
-
-//     // Close on outside click
-//     document.addEventListener('click', (e) => {
-//         const isClickInsideMenu = megaMenu.contains(e.target);
-//         const isClickOnButton = toggleMegaMenu.contains(e.target);
-
-//         if (!isClickInsideMenu && !isClickOnButton) {
-//             megaMenu.classList.add('hidden');
-//             toggleMegaMenu.parentElement.classList.add('border-transparent');
-//             toggleMegaMenu.classList.add('text-slate-600');
-//             toggleMegaMenu.classList.remove('text-slate-900');
-//         }
-//     });
-// }
-
-
 export function initHeader() {
     var toggleOpen = document.getElementById('toggleOpen');
     var toggleClose = document.getElementById('toggleClose');
@@ -111,3 +57,29 @@ export function initHeader() {
         }
     });
 }
+
+
+function toggleDropdown(id) {
+    if (!id) return;
+    const allDropdowns = document.querySelectorAll('[id^="dropdown-"]');
+    const allArrows = document.querySelectorAll('[id^="arrow-"]');
+    allDropdowns.forEach(drop => drop.classList.add('hidden'));
+    allArrows.forEach(arr => arr.classList.remove('rotate-180'));
+
+
+    const dropdown = document.getElementById(`dropdown-${id}`);
+    const arrow = document.getElementById(`arrow-${id}`);
+
+    if (dropdown.classList.contains('hidden')) {
+        dropdown.classList.remove('hidden');
+        arrow.classList.add('rotate-180');
+    } else {
+        dropdown.classList.add('hidden');
+        arrow.classList.remove('rotate-180');
+    }
+}
+
+
+document.querySelectorAll('[data-dropdown]').forEach(btn => {
+    btn.addEventListener('click', () => toggleDropdown(btn.dataset.dropdown));
+});
